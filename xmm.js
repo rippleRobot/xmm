6a87978817ee7233f8e72fac26cdd6c7fd595613
Anton Salikhmetov <anton.salikhmetov@gmail.com>
2015-07-09 19:29:33 +0300

    Revert "Reorder rippled hosts"
    This reverts commit 38b161e002248851653a7f12238e15d833a72a1a.

--- xmm.js
+++ xmm.js
@@ -5,10 +5,10 @@ var util = require("util");
 var env = process.env;
 var host = env.XMM_HOST;
 var servers = [
-	"wss://s-east.ripple.com:443",
-	"wss://s-west.ripple.com:443",
+	"wss://ripple.gatehub.net:443",
 	"wss://s1.ripple.com:443",
-	"wss://ripple.gatehub.net:443"
+	"wss://s-east.ripple.com:443",
+	"wss://s-west.ripple.com:443"
 ];
 var options = {
 	max_fee: 30000,
